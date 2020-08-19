5131673f1fb60e8046bb7bb13d714d108d4ab7ef
lihaiming <18210578206@163.com>
2020-08-19 14:38:19 +0800

    #测试git提交

---
+++ kmall-user/src/test/java/cn/lhm/kmall/kmalluser/KmallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lhm.kmall.kmalluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
